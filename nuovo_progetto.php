50baedd5b8f4f865b1ac71d3066fdfb72a615097
SimoneOcculto <simoneocculto2000@gmail.com>
2022-03-09 16:06:38 +0100

    update homepage.php add file registrazione.php update registrazione.php

---
+++ nuovo_progetto.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <form action="" method="POST">
+            <label>Nome progetto</label></br>
+            <input type="text" id="nome" name="nome" required></br>
+            <label>Descrizione</label></br>
+            <textarea id="descrizione" name="descrizione" required></textarea></br>
+            <label>Data scadenza</label></br>
+            <input type="date" id="dataScadenza" name="dataScadenza"></br>
+        </form>
+    </body>
+</html>
