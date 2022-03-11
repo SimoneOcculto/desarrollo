7bd8432db42256d019b262134c14762707ad97b8
Pepperoma00 <g.roma6@studenti.uniba.it>
2022-03-11 22:56:01 +0100

    Modificato file e risolto tutti i problemi che riguardano le task

--- nuova_task.php
+++ nuova_task.php
@@ -17,17 +17,30 @@
         $dataCreaTask = date("Y-m-d");
         $priorita = $_POST['priorita'];
 
+        switch ($priorita)
+        {
+            case "uno":
+                $priorita=1;
+                break;
+            case "due":
+                $priorita=2;
+                break;
+            case "tre":
+                $priorita=3;
+                break;
+        }
+
         //echo $mailU;
         //echo $nomeProg;
 
         $task = new db_task();
 
         if ($_POST['nome'] != "" && $_POST['descrizione'] != "") {
-            $array = array("nomeT" => $_POST['nome'],
-                "descrizioneT" => $_POST['descrizione'],
-                "data_scadenzaT" => $dataScaTask,
-                "data_creazioneT" => $dataCreaTask,
-                "priorita" => $priorita);
+            $array = array("NomeT" => $_POST['nome'],
+                "DescrizioneT" => $_POST['descrizione'],
+                "DataScadenzaT" => $dataScaTask,
+                "DataCreazioneT" => $dataCreaTask,
+                "Priorita" => $priorita);
             $task->register($array);
 
         }
@@ -54,9 +67,9 @@
     ></br>
     <label>Priorita:</label></form</br>
     <select name="priorita">
-        <option value="uno">1</option>
-        <option value="due">2</option>
-        <option value="tre">3</option>
+        <option value="uno">Bassa</option>
+        <option value="due">Media</option>
+        <option value="tre">Alta</option>
     </select>
     <input type="submit" name="invio">
 </form>
