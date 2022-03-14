e316b88d2aed6ad2bbb89852a379e400928563fa
Pepperoma00 <g.roma6@studenti.uniba.it>
2022-03-14 19:56:39 +0100

    bug fix

--- View/elenco_task.php
+++ View/elenco_task.php
@@ -1,45 +1,42 @@
 <?php
 
-require "C:/xampp/htdocs/desarrollo/Model/db_task.php";
+    require "C:/xampp/htdocs/desarrollo/Model/db_task.php";
 
-$task = new db_task();
+    $task = new db_task();
 
-$array=$task->getAllTask($_GET['id']);
+    $array=$task->getAllTask($_GET['id']);
+
 ?>
 
 <html>
-<head>
+    <head>
 
-    <h1>Elenco Task</h1>
-<body>
+        <h1>Elenco Task</h1>
 
-<?php
-if ($array == false) {
-    echo "<b>Non ci sono task</b> <a href='elenco_progetti.php'><button>Annulla</button></a>";
-} else {
-    foreach ($array as $value) {
-        echo "<table>
-                        <tr><td>
-                            " . $value->getId_task() . "
-                        </td><td>
-                            " . $value->getId_progetto() . "
-                        </td><td>
-                            " . $value->getNomeT() ."
-                        </td><td>
-                            " . $value->getDescrizioneT() ."
-                        </td><td>
-                            " . $value->getDataScadenzaT() . "
-                        </td><td>
-                           <a href='elimina_progetto.php?id=".$value->getId_task()."'><button>Elimina</button></a>
-                        </td><td>
-                           <a href='modifica_progetto.php?id=".$value->getId_task()."'><button>Modifica</button></a>
-                        </td><td>
-                           <a href='elenco_progetti.php'><button>Annulla</button></a>
-                        </td></tr>
+    </head>
+
+    <body>
+
+    <?php
+        if ($array == false) {
+            echo "<b>Non ci sono task</b> <a href='elenco_progetti.php'><button>Annulla</button></a>";
+        } else {
+            foreach ($array as $value) {
+                echo "
+                        <table>
+                            <tr>
+                                <td>".$value->getId_task()."</td>
+                                <td>".$value->getId_progetto()."</td>
+                                <td>".$value->getNomeT()."</td>
+                                <td>".$value->getDescrizioneT()."</td>
+                                <td>".$value->getDataScadenzaT()."</td>
+                                <td><a href='elimina_progetto.php?id=".$value->getId_task()."'><button>Elimina</button></a></td>
+                                <td><a href='modifica_progetto.php?id=".$value->getId_task()."'><button>Modifica</button></a></td>
+                                <td><a href='elenco_progetti.php'><button>Annulla</button></a></td>
+                            </tr>
                         </table>";
-    }
-}
-?>
-</body>
-</head>
+            }
+        }
+    ?>
+    </body>
 </html>
--- View/elimina_progetto.php
+++ View/elimina_progetto.php
@@ -1,45 +1,56 @@
 <?php
-//echo $_GET['nome'];
-require "C:/xampp/htdocs/desarrollo/Model/db_progetto.php";
+    //echo $_GET['nome'];
+    require "C:/xampp/htdocs/desarrollo/Model/db_progetto.php";
 
-$progetti = new db_progetto();
+    $progetti = new db_progetto();
 
-if(isset($_POST['elimina'])){
+    if(isset($_POST['elimina'])){
 
-    require "C:/xampp/htdocs/desarrollo/Model/db_task.php";
+        require "C:/xampp/htdocs/desarrollo/Model/db_task.php";
 
-    $task = new db_task();
-    $task->EliminaTask($_GET['id']);
-    $progetti->EliminaProgetto($_GET['id']);
+        $task = new db_task();
+        $task->EliminaTask($_GET['id']);
+        $progetti->EliminaProgetto($_GET['id']);
 
-    header('Location: C:/xampp/htdocs/desarrollo/View/elenco_progetti.php');
-
-}
-
-$array=$progetti->getArrayProgetti($_GET['id']);
-
-if ($array == false) {
-    echo "<b>Errore</b>";
-} else {
-    foreach ($array as $value) {
-        echo "<table>
-                        <tr><td>
-                            " . $value->getId() . "
-                        </td><td>
-                            " . $value->getLeader() . "
-                        </td><td>
-                            " . $value->getNomeP() ."
-                        </td><td>
-                            " . $value->getDataCreazioneP() . "
-                        </td><td>
-                           <form action='' method='POST'>
-                           <input type='submit' name='elimina' value='elimina'>
-                           </form>
-                        </td><td>
-                           <a href='elenco_progetti.php'><button>Annulla</button></a>
-                        </td></tr>
-                        </table>";
+        header('Location: elenco_progetti.php');
     }
-}
 
+    $array=$progetti->getArrayProgetti($_GET['id']);
 ?>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+
+    <?php
+        if ($array == false) {
+            echo "<b>Errore</b>";
+        } else {
+            foreach ($array as $value) {
+            echo "<table>
+                <tr><td>
+                        " . $value->getId() . "
+                    </td><td>
+                        " . $value->getLeader() . "
+                    </td><td>
+                        " . $value->getNomeP() ."
+                    </td><td>
+                        " . $value->getDataCreazioneP() . "
+                    </td><td>
+                        <form action='' method='POST'>
+                            <input type='submit' name='elimina' value='elimina'>
+                        </form>
+                    </td><td>
+                        <a href='elenco_progetti.php'><button>Annulla</button></a>
+                    </td></tr>
+            </table>";
+            }
+        }
+    ?>
+
+    </body>
+
+</html>
+
