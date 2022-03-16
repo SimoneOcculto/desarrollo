874c36b1487e7cdb6567089c11f3ea7549722d0b
gabriele <gabriele.zullo0@gmail.com>
2022-03-16 19:23:56 +0100

    frontend

--- View/elenco_task.php
+++ View/elenco_task.php
@@ -55,6 +55,11 @@
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
     </nav>
+    <div class="container">
+<span class="pull-left button-group">
+   <a class="btn btn-success" href="nuova_task.php" role="button">New Task</a>
+ </span>
+    </div>
     <?php
         if ($array == false) {
             echo "<b>Non ci sono task</b> <a href='elenco_progetti.php'><button>Annulla</button></a>";
