501168abaa8c257cb2c6e4099f88f704a4d043b8
gabriele <gabriele.zullo0@gmail.com>
2022-04-04 18:06:31 +0200

    frontend

--- View/profilo.php
+++ View/profilo.php
@@ -60,7 +60,7 @@
 
     <body>
 
-    <nav class="navbar navbar-light bg-light justify-content-between header">
+    <nav class="navbar navbar-light bg-light justify-content-between">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="index.php" style="font-size: 25px;"><b>ToDoGGS</b></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -69,27 +69,18 @@
             <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
                         <a class="nav-link" href="elenco_progetti.php">Projects</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="logout.php">logout <span class="sr-only">(current)</span></a>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profilo.php">Profile</a>
                     </li>
-                    <!-- <li class="nav-item">
-                        <a class="nav-link" href="#">Teams</a>
+                    <li class="nav-item ">
+                        <a class="nav-link" href="logout.php">Logout<span class="sr-only">(current)</span></a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown link
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>-->
+                    <li class="nav-item">
+                        <a class="nav-link" href="gestione_utenti.php">Users Management</a>
+                    </li>
+
                 </ul>
             </div>
         </nav>
@@ -97,14 +88,12 @@
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
         </form>
-
     </nav>
-
     <div class="piede">
 
         <div class="col-lg-12 text-lg-center">
             <br>
-            <h2>Profile</h2>
+            <h2>Your Profile</h2>
             <br>
 
         </div>
@@ -113,12 +102,12 @@
             <div class="supremo">
 
             <div class="riga">
-                <div class="etichetta"><label>Nome</label></div>
+                <div class="etichetta"><label>Name</label></div>
             <input class="tony" type="text" id="nome" name="nome" placeholder="Nome" value="<?php echo $result->getNome(); ?>">
             </div>
 
             <div class="riga">
-                <div class="etichetta"><label>Cognome</label></div>
+                <div class="etichetta"><label>Surname</label></div>
             <input class="tony" type="text" id="cognome" name="cognome" placeholder="Cognome" value="<?php echo $result->getCognome(); ?>">
             </div>
 
