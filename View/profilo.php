96f37a18c0f5642aec7d3c9d76894a00f501a230
gabriele <gabriele.zullo0@gmail.com>
2022-04-04 11:33:18 +0200

    dsvgszdhjvdssd

--- View/profilo.php
+++ View/profilo.php
@@ -37,10 +37,94 @@
 <!DOCTYPE html>
 <html>
     <head>
-
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <script src="//code.jquery.com/jquery.min.js"></script>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta charset="utf-8">
     </head>
 
     <body>
+    <nav class="navbar navbar-light bg-light justify-content-between">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="index.php" style="font-size: 25px;"><b>ToDoGGS</b></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="elenco_progetti.php">Projects</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="logout.php">logout <span class="sr-only">(current)</span></a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="#">Teams</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>-->
+                </ul>
+            </div>
+        </nav>
+        <form method="POST" action="search.php" class="form-inline">
+            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </nav>
+    <div class="container">
+        <!-- edit form column -->
+        <div class="col-lg-12 text-lg-center">
+            <h2>Edit Profile</h2>
+            <br>
+            <br>
+        </div>
+        <div class="container-fluid">
+            <form action="profilo.php" method="POST" role="form">
+                <div class="form-group row ">
+                    <label class="col-lg-3 col-form-label form-control-label">Name</label>
+                    <div class="col-lg-9">
+                        <input class="form-control" type="text" id="nome" name="nome" placeholder="Nome" value="<?php echo $result->getNome(); ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-lg-3 col-form-label form-control-label">Surname</label>
+                    <div class="col-lg-9">
+                        <input class="form-control" type="text" id="cognome" name="cognome" placeholder="Cognome" value="<?php echo $result->getCognome(); ?>">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-lg-3 col-form-label form-control-label">Email</label>
+                    <div class="col-lg-9">
+                        <input class="form-control" type="text" id="mail" name="mail" placeholder="Mail" value="<?php echo $_SESSION['mail']; ?>">
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label class="col-lg-3 col-form-label form-control-label"></label>
+                    <div class="col-lg-9">
+                        <input type="reset" class="btn btn-secondary" value="Cancel" />
+                        <input type="submit" class="btn btn-primary" value="Save Changes" name="modifica" />
+                    </div>
+                </div>
+            </form>
+        </div>
+
+    </div>
+    <hr />
+    <!--
         <form action="profilo.php" method="POST">
             <label>Nome</label>
             <input type="text" id="nome" name="nome" placeholder="Nome" value="<?php echo $result->getNome(); ?>">
@@ -52,6 +136,6 @@
             <input type="text" id="mail" name="mail" placeholder="Mail" value="<?php echo $_SESSION['mail']; ?>">
 
             <button type="submit" name="modifica">Modifica dati</button>
-        </form>
+        </form>-->
     </body>
 </html>
