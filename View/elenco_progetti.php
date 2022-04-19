163fe2f85b8894d427f172a430113be18afdf3f8
SimoneOcculto <simoneocculto2000@gmail.com>
2022-04-19 10:56:44 +0200

    aggiunta pulsante

--- View/elenco_progetti.php
+++ View/elenco_progetti.php
@@ -112,6 +112,7 @@
                                         <a href='modifica_progetto.php?id=<?php echo $value->getId();?>' class="btn btn-primary"><span class="glyphicon glyphicon-edit"></span> Edit</a>
                                         <a href='elenco_task.php?id=<?php echo $value->getId();?>' class="btn btn-primary"> View Tasks</a>
                                         <a href='elimina_progetto.php?id=<?php echo $value->getId();?>' class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span> Delete</a>
+                                        <a href='elenco_utenti.php?id=<?php echo $value->getId();?>' class="btn btn-primary"> Invite</a>
                                     </span>
                             </li>
                         </ul>
