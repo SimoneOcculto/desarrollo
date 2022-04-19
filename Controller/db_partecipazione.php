c0831bca7f0aa5c97975d38721a6b6f903fbe2e6
SimoneOcculto <simoneocculto2000@gmail.com>
2022-04-19 11:16:25 +0200

    aggiunta funzione

--- Controller/db_partecipazione.php
+++ Controller/db_partecipazione.php
@@ -50,14 +50,33 @@
 
         }
 
+        public function RicercaInvitiRicevutiUtente($mail){
 
+            $this->startConnection();
 
+            $sql = "SELECT * FROM partecipazione WHERE Invitato = '".$mail."';";
 
+            $result = $this->getConnection()->query($sql);
 
+            $array = array();
 
+            $this->closeconnection();
 
-
-
+            if($result) {
+                if ($result->num_rows == 0) {
+                    return false;
+                } else {
+                    for ($i = 0; $i < $result->num_rows; $i++) {
+                        $row = $result->fetch_assoc();
+                        $project = new progetto($row);
+                        $array[] = $project;
+                    }
+                }
+            } else{
+                echo "Error in ".$sql."<br>".$this->startConnection()->error;
+            }
+            return $array;
+        }
     }
 
 ?>
